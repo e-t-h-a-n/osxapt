0166b134b3057187abf7dc703401672a6a4344db
e-t-h-a-n <ethansherriff@hotmail.co.uk>
2016-06-06 10:39:56 +0100

    Update relative_libs.sh
    Made more verbose

--- relative_libs.sh
+++ relative_libs.sh
@@ -39,45 +39,57 @@ ln -s ../lib/libapt-pkg-$PKG.dylib ../lib/libapt-pkg-$PKG_MAJ.dylib
 ln -s ../lib/libapt-pkg-$PKG_MAJ.dylib ../lib/libapt-pkg.dylib
 
 ### First executable: apt. Depends on: libapt-pkg.dylib and libapt-private.dylib
+printf "\033[32;1mPatching apt\033[0m\n"
 install_name_tool -change `otool -L apt | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-pkg-$PKG.dylib apt
 install_name_tool -change `otool -L apt | grep libapt-private | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-private-$PRIVATE.dylib apt
 
+
 ### 2nd executable: apt-cache. Depends on: libapt-pkg.dylib and libapt-private.dylib
+printf "\033[32;1mPatching apt-cache\033[0m\n"
 install_name_tool -change `otool -L apt-cache | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-pkg-$PKG.dylib apt-cache
 install_name_tool -change `otool -L apt-cache | grep libapt-private | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-private-$PRIVATE.dylib apt-cache
 
 ### 3rd executable: apt-cdrom. Depends on: libapt-pkg.dylib and libapt-private.dylib
+printf "\033[32;1mPatching apt-cdrom\033[0m\n"
 install_name_tool -change `otool -L apt-cdrom | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-pkg-$PKG.dylib apt-cdrom
 install_name_tool -change `otool -L apt-cdrom | grep libapt-private | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-private-$PRIVATE.dylib apt-cdrom
 
 ### 4th executable: apt-config. Depends on: libapt-pkg.dylib and libapt-private.dylib
+printf "\033[32;1mPatching apt-config\033[0m\n"
 install_name_tool -change `otool -L apt-config | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-pkg-$PKG.dylib apt-config
 install_name_tool -change `otool -L apt-config | grep libapt-private | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-private-$PRIVATE.dylib apt-config
 
 ### 5th executable: apt-dump-solver. Depends on: libapt-pkg.dylib only
+printf "\033[32;1mPatching apt-dump-solver\033[0m\n"
 install_name_tool -change `otool -L apt-dump-solver | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-pkg-$PKG.dylib apt-dump-solver
 
 ### 6th executable: apt-extracttemplates. Depends on: libapt-pkg.dylib and libapt-inst.dylib
+printf "\033[32;1mPatching apt-extracttemplates\033[0m\n"
 install_name_tool -change `otool -L apt-extracttemplates | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-pkg-$PKG.dylib apt-extracttemplates
 install_name_tool -change `otool -L apt-extracttemplates | grep libapt-inst | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-inst-$INST.dylib apt-extracttemplates
 
 ### 7th executable: apt-get. Depends on: libapt-pkg.dylib and libapt-private.dylib
+printf "\033[32;1mPatching apt-get\033[0m\n"
 install_name_tool -change `otool -L apt-get | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-pkg-$PKG.dylib apt-get
 install_name_tool -change `otool -L apt-get | grep libapt-private | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-private-$PRIVATE.dylib apt-get
 
 ### 8th executable: apt-helper. Depends on: libapt-pkg.dylib and libapt-private.dylib
+printf "\033[32;1mPatching apt-helper\033[0m\n"
 install_name_tool -change `otool -L apt-helper | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-pkg-$PKG.dylib apt-helper
 install_name_tool -change `otool -L apt-helper | grep libapt-private | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-private-$PRIVATE.dylib apt-helper
 
 ### 9th executable: apt-internal-solver. Depends on: libapt-pkg.dylib and libapt-private.dylib
+printf "\033[32;1mPatching apt-internal-solver\033[0m\n"
 install_name_tool -change `otool -L apt-internal-solver | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-pkg-$PKG.dylib apt-internal-solver
 install_name_tool -change `otool -L apt-internal-solver | grep libapt-private | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-private-$PRIVATE.dylib apt-internal-solver
 
 ### 10th executable: apt-mark. Depends on: libapt-pkg.dylib and libapt-private.dylib
+printf "\033[32;1mPatching apt-mark\033[0m\n"
 install_name_tool -change `otool -L apt-mark | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-pkg-$PKG.dylib apt-mark
 install_name_tool -change `otool -L apt-mark | grep libapt-private | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-private-$PRIVATE.dylib apt-mark
 
 ### 11th executable: apt-sortpkgs. Depends on: libapt-pkg.dylib only
+printf "\033[32;1mPatching apt-sortpkg\033[0m\n"
 install_name_tool -change `otool -L apt-sortpkgs | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../lib/libapt-pkg-$PKG.dylib apt-sortpkgs
 
 
@@ -88,7 +100,8 @@ mv methods/ ../lib/
 cd ../lib/methods
 
 for i in ./*; do
-    install_name_tool -change `otool -L $i | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../libapt-pkg-$PKG.dylib $i
+  printf "\033[32;1mPatching "`basename $i`"\033[0m\n"
+  install_name_tool -change `otool -L $i | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/../libapt-pkg-$PKG.dylib $i
 done
 
 ### Now libraries :(((((( CRRYYYYY
@@ -97,14 +110,18 @@ cd ../
 
 for i in ./*.dylib; do
   if [ ! -h $i ]; then
+    printf "\033[32;1mPatching "`basename $i`"\033[0m\n"
     if [[ `echo \`expr \\\`otool -L $i | grep libapt- | sed 's/\t//g;s/\.dylib.*)/.dylib/g' | wc -l\\\` - 1\`` == *"libapt-pkg"* ]]; then
       install_name_tool -change `otool -L $i | grep libapt-pkg | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/libapt-pkg-$PKG.dylib $i
+      printf "\033[32;1m"`basename $i`" patched for libapt-pkg\033[0m\n"
     fi
     if [[ `echo \`expr \\\`otool -L $i | grep libapt- | sed 's/\t//g;s/\.dylib.*)/.dylib/g' | wc -l\\\` - 1\`` == *"libapt-private"* ]]; then
       install_name_tool -change `otool -L $i | grep libapt-private | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/libapt-private-$PRIVATE.dylib $i
+      printf "\033[32;1m"`basename $i`" patched for libapt-private\033[0m\n"
     fi
     if [[ `echo \`expr \\\`otool -L $i | grep libapt- | sed 's/\t//g;s/\.dylib.*)/.dylib/g' | wc -l\\\` - 1\`` == *"libapt-inst"* ]]; then
       install_name_tool -change `otool -L $i | grep libapt-inst | sed 's/\t//g;s/\.dylib.*)/.dylib/g'` @executable_path/libapt-inst-$INST.dylib $i
+      printf "\033[32;1m"`basename $i`" patched for libapt-inst\033[0m\n"
     fi
   fi
 done
